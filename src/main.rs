67b5537989097248b5812bf36325246ca2886f12
ksceriath <kxzorro@gmail.com>
2019-11-09 00:05:06 +0530

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -39,15 +39,24 @@ fn main() {
         Mismatch::NoMismatch => println!("No mismatch was found."),
         Mismatch::ValueMismatch => println!("Mismatch at root."),
         Mismatch::ObjectMismatch(None, None, None) => println!("No mismatch was found."),
-        Mismatch::ObjectMismatch(a,b,c) => {
+        Mismatch::ObjectMismatch(a, b, c) => {
             if let Some(left_keys) = a {
-                println!("Following keys were not found in second object: {:?}", left_keys);
+                println!(
+                    "Following keys were not found in second object: {:?}",
+                    left_keys
+                );
             }
             if let Some(right_keys) = b {
-                println!("Following keys were not found in first object: {:?}", right_keys);
+                println!(
+                    "Following keys were not found in first object: {:?}",
+                    right_keys
+                );
             }
             if let Some(unequal_keys) = c {
-                println!("Following keys were not found to be equal: {:?}", unequal_keys);
+                println!(
+                    "Following keys were not found to be equal: {:?}",
+                    unequal_keys
+                );
             }
         }
     };
@@ -63,7 +72,11 @@ struct KeyMap {
 enum Mismatch {
     NoMismatch,
     ValueMismatch,
-    ObjectMismatch(Option<BTreeSet<KeyMap>>, Option<BTreeSet<KeyMap>>, Option<BTreeSet<KeyMap>>),
+    ObjectMismatch(
+        Option<BTreeSet<KeyMap>>,
+        Option<BTreeSet<KeyMap>>,
+        Option<BTreeSet<KeyMap>>,
+    ),
 }
 
 fn match_json(value: &Value, value1: &Value) -> Mismatch {
@@ -72,34 +85,62 @@ fn match_json(value: &Value, value1: &Value) -> Mismatch {
             let (left, right, intersection) = intersect_maps(&a, &b);
             let mut unequal_keys = None;
 
-            let mut left = left.map(|l| l.iter().map(|x| KeyMap{ key: String::from(x), children: None }).collect::<BTreeSet<KeyMap>>());
-            let mut right = right.map(|r| r.iter().map(|x| KeyMap{ key: String::from(x), children: None }).collect::<BTreeSet<KeyMap>>());
+            let mut left = left.map(|l| {
+                l.iter()
+                    .map(|x| KeyMap {
+                        key: String::from(x),
+                        children: None,
+                    })
+                    .collect::<BTreeSet<KeyMap>>()
+            });
+            let mut right = right.map(|r| {
+                r.iter()
+                    .map(|x| KeyMap {
+                        key: String::from(x),
+                        children: None,
+                    })
+                    .collect::<BTreeSet<KeyMap>>()
+            });
 
             if let Some(intersection) = intersection {
                 for key in intersection {
-                    if let Some(keys) = match match_json(&a.get(&key).unwrap(), &b.get(&key).unwrap()) {
-                        Mismatch::NoMismatch => None,
-                        Mismatch::ValueMismatch => Some(KeyMap{ key, children: None }),
-                        Mismatch::ObjectMismatch(left_keys, right_keys, mismatch_keys) => {
-                            if let Some(left_keys) = left_keys {
-                                left.get_or_insert(BTreeSet::new()).insert(KeyMap{ key: String::from(&key), children: Some(left_keys) });
+                    if let Some(keys) =
+                        match match_json(&a.get(&key).unwrap(), &b.get(&key).unwrap()) {
+                            Mismatch::NoMismatch => None,
+                            Mismatch::ValueMismatch => Some(KeyMap {
+                                key,
+                                children: None,
+                            }),
+                            Mismatch::ObjectMismatch(left_keys, right_keys, mismatch_keys) => {
+                                if let Some(left_keys) = left_keys {
+                                    left.get_or_insert(BTreeSet::new()).insert(KeyMap {
+                                        key: String::from(&key),
+                                        children: Some(left_keys),
+                                    });
+                                }
+                                if let Some(right_keys) = right_keys {
+                                    right.get_or_insert(BTreeSet::new()).insert(KeyMap {
+                                        key: String::from(&key),
+                                        children: Some(right_keys),
+                                    });
+                                }
+                                if let Some(mismatch_keys) = mismatch_keys {
+                                    Some(KeyMap {
+                                        key: String::from(&key),
+                                        children: Some(mismatch_keys),
+                                    })
+                                } else {
+                                    None
+                                }
                             }
-                            if let Some(right_keys) = right_keys {
-                                right.get_or_insert(BTreeSet::new()).insert(KeyMap{ key: String::from(&key), children: Some(right_keys) });
-                            }
-                            if let Some(mismatch_keys) = mismatch_keys {
-                                Some(KeyMap{ key: String::from(&key), children: Some(mismatch_keys) })
-                            } else {
-                                None
-                            }
-                        },
-                    } {
+                        }
+                    {
                         unequal_keys.get_or_insert(BTreeSet::new()).insert(keys);
                     }
                 }
             }
             Mismatch::ObjectMismatch(left, right, unequal_keys)
-        },
+        }
         (a, b) => {
             if a == b {
                 Mismatch::NoMismatch
@@ -110,9 +151,14 @@ fn match_json(value: &Value, value1: &Value) -> Mismatch {
     }
 }
 
-fn intersect_maps<'a>(a: &Map<String, Value>, 
-    b: &Map<String, Value>) -> (Option<BTreeSet<String>>,
-    Option<BTreeSet<String>>, Option<BTreeSet<String>>) {
+fn intersect_maps(
+    a: &Map<String, Value>,
+    b: &Map<String, Value>,
+) -> (
+    Option<BTreeSet<String>>,
+    Option<BTreeSet<String>>,
+    Option<BTreeSet<String>>,
+) {
     let mut intersection = BTreeSet::new();
     let mut left = BTreeSet::new();
     let mut right = BTreeSet::new();
@@ -130,6 +176,10 @@ fn intersect_maps<'a>(a: &Map<String, Value>,
     }
     let left = if left.len() == 0 { None } else { Some(left) };
     let right = if right.len() == 0 { None } else { Some(right) };
-    let intersection = if intersection.len() == 0 { None } else { Some(intersection) };
+    let intersection = if intersection.len() == 0 {
+        None
+    } else {
+        Some(intersection)
+    };
     (left, right, intersection)
 }
